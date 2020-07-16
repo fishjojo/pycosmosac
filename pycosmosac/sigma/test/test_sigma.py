27f9ae90bcfd2f4bb21a32b6e5075a780220835d
Xing Zhang <zhangxing.nju@gmail.com>
2020-07-16 02:23:17 -0700

    add test for dumping sigma file

--- pycosmosac/sigma/test/test_sigma.py
+++ pycosmosac/sigma/test/test_sigma.py
@@ -18,5 +18,13 @@ class KnownValues(unittest.TestCase):
     def test_sigma(self):
         self.assertAlmostEqual(misc.fp(mysigma.pA), -1.917622937152116, 8)
 
+    def test_dump(self):
+        mysigma.dump_to_file("/tmp/tmp.sigma")
+        with open("/tmp/tmp.sigma", 'r') as f:
+            contents = f.read().splitlines()
+            sigma, pA = contents[12].split()
+            self.assertAlmostEqual(float(sigma), -0.0160, 9)
+            self.assertAlmostEqual(float(pA), 1.63558853113542, 9)
+
 if __name__ == '__main__':
     unittest.main()
