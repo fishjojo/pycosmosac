0bd4de361d2948cef9f8679c8d2d0fab8244faa7
Xing Zhang <zhangxing.nju@gmail.com>
2020-07-24 17:27:27 -0700

    add test for antoine

--- pycosmosac/utils/test/test_antoine.py
+++ pycosmosac/utils/test/test_antoine.py
@@ -2,6 +2,11 @@ import unittest
 from pycosmosac.utils import antoine
 
 class KnownValues(unittest.TestCase):
+    def test_antoine_nist(self):
+        data = antoine.get_antoine_nist("7732-18-5")
+        P = antoine.antoine_to_vapor(data, 300)
+        self.assertAlmostEqual(P, 3554.6837736037555, 9)
+
     def test_vapor(self):
         data = [[[250.04, 328.57], [350.14, 466.73], [212.4, 293.02]], 
                 [4.022, 4.46988, 4.13377], 
